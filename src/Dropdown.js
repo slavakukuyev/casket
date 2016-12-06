cd1c39aa26e7dff7a95d170c5befdc7a42ac5a24
Slava Kukuyev <slava.kukuyev@gmail.com>
2016-12-06 21:29:48 +0200

    Create basic rendering

---
+++ src/Dropdown.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class Dropdown extends Component {
+constructor(props) {
+	super(props);
+	this.state ={isOpened: false};
+}
+
+toggleState(){
+	this.setState({isOpened: !this.state.isOpened});
+}
+
+  render() {
+  	console.log('isOpened', this.state.isOpened);
+    return (
+      <div onClick={this.toggleState.bind(this)}>shdfhgd dhf fg hdfghd</div>
+    );
+  }
+}
+
+export default Dropdown;
