8455b9fba9fde27e1c5b9ddff3b5f456e95a29a4
Slava Kukuyev <slava.kukuyev@gmail.com>
2016-12-13 23:54:01 +0200

    Props in React

--- src/Dropdown.js
+++ src/Dropdown.js
@@ -10,10 +10,21 @@ toggleState(){
 	this.setState({isOpened: !this.state.isOpened});
 }
 
+
+
   render() {
   	console.log('isOpened', this.state.isOpened);
+
+	let dropdownText;
+	if(this.state.isOpened) {
+		dropdownText = <div>There is a dropdown text</div>;
+	}
+  	
     return (
-      <div onClick={this.toggleState.bind(this)}>shdfhgd dhf fg hdfghd</div>
+      <div onClick={this.toggleState.bind(this)}>
+      shdfhgd dhf fg hdfghd
+		{dropdownText}
+      </div>
     );
   }
 }
